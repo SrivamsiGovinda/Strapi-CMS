8a57b3fd2668caca9b055430abe76690f7271858
Srivamsi <srivamsivamsi90@gmail.com>
2025-07-30 19:11:19 +0530

    Removed Github IAM role and Added AWS_ACCESS_KEY and SECRET_KEY

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -10,13 +10,57 @@ variable "github_repo" {
     default = "SrivamsiGovinda/Strapi-CMS"
 }
 
-#VPC Module
+# VPC Module
 module "vpc" {
-    source = "../../modules/vpc"
+  source = "./modules/vpc"
 }
 
-#Aurora Module
+# Aurora Module
 module "aurora" {
-    source = "../../modules/aurora"
-    vpc_id = module.vpc.vpc_id
+  source           = "./modules/aurora"
+  vpc_id           = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+  ecs_security_group_id = module.ecs.ecs_security_group_id
+}
+
+# ECS Module
+module "ecs" {
+  source            = "./modules/ecs"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  aurora_endpoints  = module.aurora.endpoints
+  aurora_db_names   = module.aurora.db_names
+  aurora_secrets    = module.aurora.secrets
+}
+
+# Route53 Module
+module "route53" {
+  source         = "./modules/route53"
+  domain_name    = var.domain_name
+  prod_alb_dns   = module.ecs.prod_alb_dns
+  staging_alb_dns = module.ecs.staging_alb_dns
+}
+
+# ECR Module
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+# IAM Module
+module "iam" {
+  source = "./modules/iam"
+}
+
+output "aws_access_key_id" {
+  value     = module.iam.access_key_id
+  sensitive = true
+}
+
+output "aws_secret_access_key" {
+  value     = module.iam.secret_access_key
+  sensitive = true
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
 }
--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,37 +1,13 @@
-# IAM Role for GitHub Actions OIDC
-resource "aws_iam_role" "github_actions" {
-  name = "github-actions-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
-          }
-        }
-      }
-    ]
-  })
+resource "aws_iam_user" "github_actions" {
+  name = "github-actions-user"
+  tags = {
+    Name = "strapi-github-actions-user"
+  }
 }
 
-resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-
-resource "aws_iam_role_policy" "github_actions_policy" {
+resource "aws_iam_user_policy" "github_actions_policy" {
   name = "github-actions-policy"
-  role = aws_iam_role.github_actions.id
+  user = aws_iam_user.github_actions.name
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -53,6 +29,16 @@ resource "aws_iam_role_policy" "github_actions_policy" {
   })
 }
 
-output "github_actions_role_arn" {
-  value = aws_iam_role.github_actions.arn
+resource "aws_iam_access_key" "github_actions" {
+  user = aws_iam_user.github_actions.name
+}
+
+output "access_key_id" {
+  value     = aws_iam_access_key.github_actions.id
+  sensitive = true
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.github_actions.secret
+  sensitive = true
 }
