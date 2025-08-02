125b4d7f0dd2307f0a6ff2647d713c42c1c380f7
Srivamsi <srivamsivamsi90@gmail.com>
2025-08-02 05:54:38 +0530

    Added terraform statefile

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "strapi-terraform-state"
+    key            = "strapi/state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "strapi-terraform-lock"
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
