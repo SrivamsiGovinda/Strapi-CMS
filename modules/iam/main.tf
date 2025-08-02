f9fbd62bd2b606ed9c096ef21a76fbf683cc7daa
Srivamsi <srivamsivamsi90@gmail.com>
2025-08-03 00:20:10 +0530

    Added Managed policy

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -5,9 +5,8 @@ resource "aws_iam_user" "github_actions" {
   }
 }
 
-resource "aws_iam_user_policy" "github_actions_policy" {
+resource "aws_iam_policy" "github_actions_policy" {
   name = "github-actions-policy"
-  user = aws_iam_user.github_actions.name
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
